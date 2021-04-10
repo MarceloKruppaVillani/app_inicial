f314204ac9dea7cf8fba51d9a8c431bef9fb590b
MarceloKruppaVillani <marcelo.kruppa@gmail.com>
2021-04-10 20:37:23 +0000

    Inclusão de Templates e uso da Jinja2 na renderização de Templates

--- app_inicio.py
+++ app_inicio.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 appl = Flask(__name__)
 
 @appl.route("/")
 def inicio():
-    return ("<h1>Hello!</h1>")
+    return render_template("home.html", titulo = "Home")
     
 if __name__ == '__main__':
 	appl.run(host='0.0.0.0', port=8080, debug=True)
